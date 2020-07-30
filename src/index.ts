6416cbb535cec7ad4ab4a7755a1ec31561010195
keng42 <me@keng42.com>
2020-07-30 11:27:27 +0800

    feat: export all functions as a single object

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ export * from './number';
 export * from './date';
 export * from './type';
 export * from './sleep';
+
+import * as number from './number';
+import * as date from './date';
+import * as type from './type';
+import * as sleep from './sleep';
+
+export default {
+  ...number,
+  ...date,
+  ...type,
+  ...sleep,
+};
